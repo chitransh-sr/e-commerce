755db7e1cc9ec6f051267ca3463698d2259d4428
Chitransh Srivastava <73604474+chitransh-sr@users.noreply.github.com>
2025-03-26 19:30:47 +0530

    add: product details page added (#16)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Categories from './components/category/Category';
-import Products from './components/product/Product';
+import Products from './components/product-card/product-card';
 import Header from './components/header/Header';
 import Cart from './components/cart/Cart';
 import NewsletterSubscription from './components/newsletter/Newsletter';
 import CarauselApp from './components/carousel/Carousel';
 import Footer from './components/footer/Footer'
+import ProductDetail  from './components/product-detail/product-detail';
 
 const HomeLayout = () => (
   <>
@@ -26,7 +27,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomeLayout />} />
-        <Route path="/products" element={<HomeLayout />} />
+        <Route path="/product" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart/>}/>
       </Routes>
     </Router>
